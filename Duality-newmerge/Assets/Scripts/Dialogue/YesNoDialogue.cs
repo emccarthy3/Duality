f7e192d2c7d9688bc2c17d633d66f5b2a5622f1f
Elizabeth McCarthy <emccarthy3@carpenter-01.elon.edu>
2016-11-08 18:04:01 -0500

    added dialogue

--- Duality-newmerge/Assets/Scripts/Dialogue/YesNoDialogue.cs
+++ Duality-newmerge/Assets/Scripts/Dialogue/YesNoDialogue.cs
@@ -1,23 +1,49 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
-public class YesNoDialogue : MonoBehaviour {
-
-	private string [] questions = {"Do you like the color orange?",
-									"Do you like this game?",
-		"Thanksgiving is coming up... do you think Bush did 9/11?"};
+public class YesNoDialogue : MonoBehaviour
+{
+	//i is what question the player is on
+	private int i = 0;
+	private string[] questions = {"Do you like the color orange?",
+		"Do you like this game?",
+		"Thanksgiving is coming up... do you think Bush did 9/11?",
+		"Are you having fun in Game Programming?",
+		"Would you kill someone for an A?",
+		"Would you kill someone for an A in Game Programming?"
+	};
+	private Text question;
+	private Button button;
 
 	// Use this for initialization
 	void Start () {
-	
+		GameObject textObject = GameObject.Find ("Question");
+		question = textObject.GetComponent<Text> ();
+		question.text = questions [i]; 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (i == questions.Length) {
+			question.text = "All done!";
+		}
 	}
 
-	public void OnClick () {
+	public void OnPostiveClick () {
+		i++;
+		question.text = "Great! Also, " + questions [i];
+		Debug.Log ("Increase RP by 1");
 
 	}
+
+	public void OnNegativeClick () {
+
+		i++;
+		question.text = "Disgusting! Also, " + questions [i]; 
+		Debug.Log ("Decrease RP by 1");
+
+	}
+
+
 }
