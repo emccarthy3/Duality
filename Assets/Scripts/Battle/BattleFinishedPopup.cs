09e13aa914b47ac2416ec601cc63c78ffa91bde7
Evan Elkin <eelkin@carpenter-04.elon.edu>
2016-12-04 22:29:07 -0500

    did splash screen

---
+++ Assets/Scripts/Battle/BattleFinishedPopup.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BattleFinishedPopup : MonoBehaviour {
+	[SerializeField] private GameObject button;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+	public void Open(){
+		gameObject.SetActive (true);
+
+	}
+
+	public void Close(){
+		gameObject.SetActive (false);
+	}
+}
---
+++ Assets/Scripts/Menu/StartMenu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartMenu : MonoBehaviour {
+	[SerializeField] private Button startButton;
+	[SerializeField] private Button storyButton;
+	[SerializeField] private Panel storyPanel;
+	// Use this for initialization
+	void Start () {
+		startButton.onClick.AddListener(() =>  SwitchScenes());
+		storyButton.onClick.AddListener(() =>  ShowStory());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+		
+	public void SwitchScenes(){
+		SceneManager.LoadScene ("newBattle");
+	}
+
+	public void ShowStory(){
+
+	}
+
+}
